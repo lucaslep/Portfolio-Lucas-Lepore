0f2bbdd1c07cacb22f7bb7504d7778d8a0db30d0
Lucas Lepore <lucaslep2512@gmail.com>
2025-07-23 17:26:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function enviarWhats(event){
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5511972802912';
+
+    const texto = `Olá! Me chamo ${nome}, ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto);
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+    window.open(url, '_blank');
+}
